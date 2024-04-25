934a7d2354ed9e79e2f5ab03640b4a5259ffafc6
Mosiur Rahman Sweet <sweet.csevu@gmail.com>
2024-04-25 16:15:34 +0600

    Initial Commit

---
+++ 1_your_first_program.dart
@@ -0,0 +1,16 @@
+/*
+This is a simple dart program that prints Hello World on terminal. 
+It's a norm to write Hello World program as a developer's first program.
+*/
+
+void main() {
+  print("Hello World!");
+}
+
+/* 
+✔️ Every program must start with a main function.
+✔️ void main() is the starting point where the execution of your program begins.
+✔️ The curly braces {} represent the beginning and the ending of a block of code.
+✔️ print(“Hello World!”); prints Hello World! on screen.
+✔️ Each code statement must end with a semicolon.
+*/
